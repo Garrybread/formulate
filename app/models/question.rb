5e9f9575d8022c5940b4951557a907fe2cc4fa2f
Garrybread <ryanstevencooper@gmail.com>
2022-07-18 20:03:55 +0000

    Made questions belong to a question type

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,10 @@
 class Question < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   
   validates :prompt, presence: true
 
+  belongs_to_active_hash :question_type
   belongs_to :form
   has_many :question_responses, dependent: :destroy
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_201713) do
+ActiveRecord::Schema.define(version: 2022_07_18_184412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_07_01_201713) do
     t.bigint "form_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "question_type_id"
     t.index ["form_id"], name: "index_questions_on_form_id"
   end
 
